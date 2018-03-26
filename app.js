ba8d8b691fb2e9de90dbd674717831f837d801f6
Zeeshan Tamboli <zeeshan.tamboli@gmail.com>
2018-03-26 10:22:06 +0530

    Creating a new project

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+//Static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+const port = 3000;
+app.listen(port, () => console.log(`Server started on port ${port}`));
