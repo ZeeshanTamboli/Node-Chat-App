c71c53d2b93a6895630f889e6a0fc1ebd29996f6
Zeeshan Tamboli <zeeshan.tamboli@gmail.com>
2018-03-30 20:33:34 +0530

    Greet new users and alert others

--- app.js
+++ app.js
@@ -10,16 +10,35 @@ app.use(express.static(path.join(__dirname, 'public')));
 io.on('connection', socket => {
   console.log('New user connected');
 
+  //socket.emit from Admin
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New User joined',
+    createdAt: new Date().getTime()
+  });
+
   //Listener - createMessage
   socket.on('createMessage', message => {
     console.log('createMessage', message);
-
     //Send to all users
     io.emit('newMessage', {
       from: message.from,
       text: message.text,
       createdAt: new Date().getTime()
     });
+
+    //Emit message to other users but not itself
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
   socket.on('disconnect', () => {
