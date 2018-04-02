00e7cef046a01dfcb5a57cf2d2c7fade1580482a
Zeeshan Tamboli <zeeshan.tamboli@gmail.com>
2018-04-02 22:27:49 +0530

    Timestamps and Formatting with Moment

---
+++ playground/time.js
@@ -0,0 +1,8 @@
+const moment = require('moment');
+
+// const date = moment();
+// date.add(100, 'y').subtract(9, 'M');
+// console.log(date.format('MMM Do, YYYY'));
+
+const date = moment();
+console.log(date.format('h:mm a'));
