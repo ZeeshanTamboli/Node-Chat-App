d6147f67436efe67fb00a277fdfd86b8b1a06577
Zeeshan Tamboli <zeeshan.tamboli@gmail.com>
2018-04-13 23:37:33 +0530

    Configuration to deploy to heroku

--- app.js
+++ app.js
@@ -92,5 +92,5 @@ io.on('connection', socket => {
   });
 });
 
-const port = 3000 || process.env.PORT;
+const port = process.env.PORT || 3000;
 server.listen(port, () => console.log(`Server started on port ${port}`));
